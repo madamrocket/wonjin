536cbbe6ae7ead7b58d4335f010b3bbabe5fce5c
appear <appear.ko@gmail.com>
2020-03-15 18:33:52 +0900

    :zap: GNB logo 를 추가합니다

--- src/components/gnb/index.tsx
+++ src/components/gnb/index.tsx
@@ -1,10 +1,27 @@
 import { Link } from 'gatsby'
 import React from 'react'
+import styled from 'styled-components'
 
 import Container from '../share/container'
+import { media } from '../share/media'
+
+const Logo = styled.img`
+  width: 306px;
+  height: 80px;
+  margin-left: 160px;
+  display: block;
+
+  @media ${media.md} {
+    margin-left: 54px;
+  }
+`
 
 function GNB() {
-  return <Container>GNB</Container>
+  return (
+    <Container padding="12px 0">
+      <Logo src="/images/logo-gnb@3x.png" />
+    </Container>
+  )
 }
 
 export default GNB
--- src/components/share/media.tsx
+++ src/components/share/media.tsx
@@ -3,6 +3,6 @@ export type Media = 'md' | 'lg'
 export const media: {
   [key in Media]: string
 } = {
-  md: '(min-width: 1024px)',
-  lg: '(min-width: 1920px)',
+  md: '(max-width: 1024px)',
+  lg: '(max-width: 1920px)',
 }
