18110572269a60f3c627ba62473f5e65015ad0a7
madamrocket_ imac_01 <emmi@wonderroute.com>
2020-04-08 16:49:02 +0900

    task_센터명수정2

--- src/__generated__/gatsby-types.ts
+++ src/__generated__/gatsby-types.ts
@@ -2639,6 +2639,11 @@ export type StringQueryOperatorInput = {
   readonly glob: Maybe<Scalars['String']>;
 };
 
+export type Unnamed_1_QueryVariables = {};
+
+
+export type Unnamed_1_Query = { readonly site: Maybe<{ readonly siteMetadata: Maybe<Pick<SiteSiteMetadata, 'title' | 'description'>> }> };
+
 export type SiteTitleQueryQueryVariables = {};
 
 
@@ -2691,11 +2696,6 @@ export type GatsbyImageSharpSizes_withWebp_tracedSVGFragment = Pick<ImageSharpSi
 export type GatsbyImageSharpSizes_noBase64Fragment = Pick<ImageSharpSizes, 'aspectRatio' | 'src' | 'srcSet' | 'sizes'>;
 
 export type GatsbyImageSharpSizes_withWebp_noBase64Fragment = Pick<ImageSharpSizes, 'aspectRatio' | 'src' | 'srcSet' | 'srcWebp' | 'srcSetWebp' | 'sizes'>;
-
-export type Unnamed_1_QueryVariables = {};
-
-
-export type Unnamed_1_Query = { readonly site: Maybe<{ readonly siteMetadata: Maybe<Pick<SiteSiteMetadata, 'title' | 'description'>> }> };
 
 export type PagesQueryQueryVariables = {};
 
